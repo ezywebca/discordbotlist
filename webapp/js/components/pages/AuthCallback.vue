430c33848c23b5a9427883f3533d30641c9321ec
Yousef Sultan <yousef.su.2000@gmail.com>
2018-08-12 11:57:31 +0300

    Add debugging code for mistmatch thingy
    Signed-off-by: Yousef Sultan <yousef.su.2000@gmail.com>

--- webapp/js/components/pages/AuthCallback.vue
+++ webapp/js/components/pages/AuthCallback.vue
@@ -18,9 +18,11 @@
 			const code = getURLParameter('code');
 			const state = getURLParameter('state');
 
+			console.log(code, state);
+
 			if (code) {
 				const originalState = localStorage.getItem('discord_oauth_state');
-
+				console.log(originalState);
 				if (!originalState || originalState !== state) {
 					this.status = 'State mismatch error.';
 				} else {
